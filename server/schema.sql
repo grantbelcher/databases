56d95cb54f564dd8e2f4ac76bc9b92e408276cf9
grantbelcher <gtbelch@gmail.com>
2020-02-26 17:34:35 -0800

    Initial commit

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,18 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  /* Describe your table here.*/
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20)
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  text VARCHAR(140),
+  FOREIGN KEY(user_id)
+       REFERENCES users(user_id)
 );
 
 /* Create other tables and define schemas for them here! */
