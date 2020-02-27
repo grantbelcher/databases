9dd2e7b63b306a7c5f3837cf43a01aca33689bb1
grantbelcher <gtbelch@gmail.com>
2020-02-26 20:55:35 -0800

    updates client side programs

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,10 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
-
+var con = mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student'
+  database: 'chat'
+});
 
